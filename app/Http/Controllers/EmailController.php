f5ed6787f82328bc00aabe17383ea8bdc5acef39
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2021-02-01 15:29:50 +0700

    send gmail laravel 8

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\FirstEmail;
+
+
+class EmailController extends Controller
+{
+    public function sendEmail() {
+
+        $to_email = "hellowasis@gmail.com";
+
+        Mail::to($to_email)->send(new FirstEmail);
+
+        return "<p> Success! Your E-mail has been sent.</p>";
+
+    }
+}
